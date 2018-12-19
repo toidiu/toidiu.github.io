e9f1a47e2d8b751e674fb257176bb8be63628b6d
toidiu <apoorv@toidiu.com>
2018-12-19 02:35:29 -0800

    prevent event on more click

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,11 @@
 
 $(document).ready(function(){
   $('.more').click(function() {
+    event.preventDefault();
     $( this ).parent().toggleClass("hide-more")
   })
   $('.less').click(function() {
+    event.preventDefault();
     $( this ).parent().toggleClass("hide-more")
   })
 
